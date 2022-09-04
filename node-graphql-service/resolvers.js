efe6971962386d58cc508518cbf726b1a022287e
ildas <il.iyadaskalov8@gmail.com>
2022-09-04 14:51:57 +0300

    renaming service

--- node-graphql-service/resolvers.js
+++ node-graphql-service/resolvers.js
@@ -4,6 +4,7 @@ const resolvers = {
             return 'Hello World';
         },
         getProducts: () => {
+            // TODO use grpc from store-manager-service.js
             return [
                 {id: 14, name: "Swimwear", color: "Red", price: 53.71, categoryId: 7},
                 {id: 15, name: "Swimwear", color: "Red", price: 53.71, categoryId: 7}
